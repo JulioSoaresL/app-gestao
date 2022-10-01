48e28830244cce9fa7df4124f71bd0ad9d86063c
JulioSoaresL <juliosoareslima@outlook.com>
2022-09-30 23:35:26 -0300

    estilizando views(modelo 1)

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,22 @@
 <?php
 
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return "Login"; })->name('site.login');
 
-Route::get('/', [PrincipalController::class, 'principal']);
-Route::get('/sobrenos', [SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return "clientes"; })->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', function(){ return "produtos"; })->name('app.produtos');
+});
+
+Route::fallback(function(){
+    return "A página solicitada está indisponível";
+});
