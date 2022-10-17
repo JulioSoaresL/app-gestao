42b4e8d13ec7d4d3d110b830c35a1f4bf0374209
JulioSoaresL <juliosoareslima@outlook.com>
2022-10-17 17:41:48 -0300

    gravando dados no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
+        /* dd($request->all());
+        echo $request->input('nome');
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        print_r($contato->getAttributes());
+        $contato->save(); */
+        
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        
+
+
         return view('site.contato');
     }
 }
