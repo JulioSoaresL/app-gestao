3023441587d58b66434778062168de18a9f4d09f
JulioSoaresL <juliosoareslima@outlook.com>
2022-10-31 18:26:18 -0300

    implementando login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
@@ -14,7 +15,8 @@ Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contat
 
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login', function(){ return "Login"; })->name('site.login');
+Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:ldap, visitante')->prefix('/app')->group(function(){
     Route::get('/clientes', function(){ return "clientes"; })->name('app.clientes');
