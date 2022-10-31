5340338b881487b74d56a2f182e0dae15a4f922a
JulioSoaresL <juliosoareslima@outlook.com>
2022-10-31 19:43:24 -0300

    adicionando fornecedores no DB

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
@@ -16,8 +17,30 @@ class FornecedorController extends Controller
         return view('app.fornecedor.listar');
     }
 
-    public function adicionar()
+    public function adicionar(Request $request)
     {
+        if ($request->input('_token') != '') {
+            $regras = [
+                'nome' => 'required | min:3 | max:40',
+                'site' => 'required',
+                'uf' => 'required | min:2 | max: 2',
+                'email' => 'email',
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute é obrigatório',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no mínimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no mínimo 2 caracteres',
+                'email' => 'O email deve ser válido',
+            ];
+
+            $request->validate($regras, $feedback);
+            
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+        }
         return view('app.fornecedor.adicionar');
     }
 }
