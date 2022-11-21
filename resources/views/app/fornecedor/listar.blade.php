b577423f59790a0ff1c2b1a634808c3362e3b7a1
JulioSoaresL <juliosoareslima@outlook.com>
2022-11-20 22:46:29 -0300

    add pesquisa de fornecedores

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -17,8 +17,31 @@
         </div>
 
         <div class="informacao-pagina">
-            <div style="width:30%; margin: auto;">
-                lista
+            <div style="width:90%; margin: auto;">
+                <table border="1px" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Site</th>
+                            <th>UF</th>
+                            <th>Email</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($fornecedores as $fornecedor)
+                            <tr>
+                                <td>{{ $fornecedor->nome }}</td>
+                                <td>{{ $fornecedor->site }}</td>
+                                <td>{{ $fornecedor->uf }}</td>
+                                <td>{{ $fornecedor->email }}</td>
+                                <td>Excluir</td>
+                                <td>Editar</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
